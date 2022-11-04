1bb854191bc43041c19f178e327136f037400a6f
daniellanorris <daniellanorris@ufl.edu>
2022-11-04 15:31:50 -0400

    changes

--- index.js
+++ index.js
@@ -1,65 +1,99 @@
 var APIkey = "c5a3850c1e43b5e0245b77aa61458c8a"
 var input = document.querySelector('input')
-
 var div = document.createElement('div')
 var weatherSection = document.getElementById('weather')
 var form = document.querySelector('form')
 var weatherApp = document.getElementById('weather-app')
+var label = document.querySelector('label')
+var weatherSearch = document.getElementById('weather-search')
 
 
 form.onsubmit = function (e) {
+
     e.preventDefault()
-    weatherApp.prepend(div)
+    weatherSection.prepend(div)
+    var URL = "http://api.openweathermap.org/data/2.5/weather?q="
     var city = this.search.value.trim()
-    var URL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
-    if (!city) {
-        div.textContent = "Location Not Found";
-        return
-    } else {
-        div.textContent = ""
-        var label = document.querySelector('label')
-        
+    var fullURL = URL + city + "&appid=" + APIkey;
+    if ((!city) || (weatherSearch.value = '')) {
+        city = ''
+        div.innerHTML = ''
+        weatherSearch.value = ''
     }
-    fetch(URL)
+    fetch(fullURL)
         .then(function (res) {
+            if (res.status !== 200) throw new Error('Location not Found')
             return res.json()
         })
         .then(renderData)
-    label.textContent = ""
+
+        .catch(function (err) {
+            div.innerHTML = err.message
+        })
 
 }
 
 function renderData(data) {
-//CITY AND COUNTRU
+    city = ""
+    div.innerHTML = ""
+    weatherSearch.value = ''
+
+    //CITY AND COUNTRY
     console.log(data)
+
     var cityState = document.createElement('h2')
     div.appendChild(cityState)
     cityState.textContent = data.name + " , " + data.sys.country
     var lat = data.coord.lat
     var lon = data.coord.lon
-//MAP LINK
+
+    //MAP LINK
     var hyperlink = document.createElement('a')
-    var locationMap = "https://www.google.com/maps/search/?api=1&query=" + lat + "%2C" + lon
+    var locationMap = "https://www.google.com/maps/search/?api=1&query=" + lat + "," + lon
     div.appendChild(hyperlink)
     hyperlink.textContent = "Click to View Map"
     hyperlink.href = locationMap
-//CONDITION ICON
+    hyperlink.target = "_BLANK"
 
-//DISPLAY CONDITION
+    //CONDITION ICON
 
-//CURRENT TEMP
+    var img = document.createElement('img')
+    var iconCode = data.weather[0].icon
+    var iconURL = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png"
+    img.src = iconURL
+    img.alt = data.name
+    div.appendChild(img)
 
-//CURRENT 'FEELS LIKE'
+    //DISPLAY CONDITION
+    var condition = document.createElement('p')
+    condition.setAttribute('style', 'text-transform: capitalize')
+    condition.textContent = data.weather[0].description
+    div.appendChild(condition)
 
-//UPDATED TIME
+    //CURRENT TEMP
+    var temp = document.createElement('p')
+    tempNumber = ((data.main.temp - 273.15) * 9 / 5 + 32).toFixed(2)
+    temp.textContent = "Current:  " + tempNumber + '° F'
+    div.appendChild(temp)
 
-//SHOULD NOT DISPLAY PREVIOUS LOCATION'S WEATHER INFO AFTER SEARCHING FOR NEW
+    //CURRENT 'FEELS LIKE'
+    var feelsLike = document.createElement('p')
+    feelsLikeNumber = ((data.main.feels_like - 273.15) * 9 / 5 + 32).toFixed(2)
+    feelsLike.textContent = "Feels like:  " + feelsLikeNumber + '° F'
+    div.appendChild(feelsLike)
+
+    //UPDATED TIME
+    var dateParagraph = document.createElement('p')
+    var date = new Date((data.dt) * 1000)
+    var timeString = date.toLocaleTimeString('en-US', {
+        hour: 'numeric',
+        minute: '2-digit'
+    })
+    dateParagraph.textContent = 'Last Updated:  ' + timeString
+    div.appendChild(dateParagraph)
 
 
-    }
-   
-    
 
-
+}
 
 
