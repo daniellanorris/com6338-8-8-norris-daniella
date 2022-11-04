1d1234e828949430b9ac1341c0ffe03deb1459a1
daniellanorris <daniellanorris@ufl.edu>
2022-11-04 04:39:14 -0400

    donegit add index.js

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ form.onsubmit = function (e) {
     var URL = "http://api.openweathermap.org/data/2.5/weather?q="
     var city = this.search.value.trim()
     var fullURL = URL + city + "&appid=" + APIkey;
-    if((!city )|| (weatherSearch.value = '')) {
+    if ((!city) || (weatherSearch.value = '')) {
         city = ''
         div.innerHTML = ''
         weatherSearch.value = ''
-        }
+    }
     fetch(fullURL)
         .then(function (res) {
             if (res.status !== 200) throw new Error('Location not Found')
@@ -46,6 +46,7 @@ function renderData(data) {
     cityState.textContent = data.name + " , " + data.sys.country
     var lat = data.coord.lat
     var lon = data.coord.lon
+
     //MAP LINK
     var hyperlink = document.createElement('a')
     var locationMap = "https://www.google.com/maps/search/?api=1&query=" + lat + "," + lon
@@ -56,15 +57,41 @@ function renderData(data) {
 
     //CONDITION ICON
 
+    var img = document.createElement('img')
+    var iconCode = data.weather[0].icon
+    var iconURL = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png"
+    img.src = iconURL
+    img.alt = data.name
+    div.appendChild(img)
+
     //DISPLAY CONDITION
+    var condition = document.createElement('p')
+    condition.setAttribute('style', 'text-transform: capitalize')
+    condition.textContent = data.weather[0].description
+    div.appendChild(condition)
 
     //CURRENT TEMP
+    var temp = document.createElement('p')
+    tempNumber = ((data.main.temp - 273.15) * 9 / 5 + 32).toFixed(2)
+    temp.textContent = "Current:  " + tempNumber + '° F'
+    div.appendChild(temp)
 
     //CURRENT 'FEELS LIKE'
+    var feelsLike = document.createElement('p')
+    feelsLikeNumber = ((data.main.feels_like - 273.15) * 9 / 5 + 32).toFixed(2)
+    feelsLike.textContent = "Feels like:  " + feelsLikeNumber + '° F'
+    div.appendChild(feelsLike)
 
     //UPDATED TIME
+    var dateParagraph = document.createElement('p')
+    var date = new Date((data.dt) * 1000)
+    var timeString = date.toLocaleTimeString('en-US', {
+        hour: 'numeric',
+        minute: '2-digit'
+    })
+    dateParagraph.textContent = 'Last Updated:  ' + timeString
+    div.appendChild(dateParagraph)
 
-    //SHOULD NOT DISPLAY PREVIOUS LOCATION'S WEATHER INFO AFTER SEARCHING FOR NEW
 
 
 }
